16f62f6443ebb153a7da189e30435949e512e9b9
Joseph Dao <joedao55@gmail.com>
2020-07-01 20:08:07 -0400

    Update example.cpp
    fixed includes

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,6 +1,6 @@
-#include "matrix.hpp"
+#include "..include/matrix.hpp"
 
-#include "operator.hpp"
+#include "..include/operator.hpp"
 int main()
 {
     constexpr ce::ctmatrix<int, 3, 3> test{ 0, 1, 2, 3, 4, 5, 6, 7, 8 };
