cbcc9c977b151e53705a465b7b4ba2e32508f25d
joe dao <joedao55@gmail.com>
2020-07-05 13:16:17 -0400

    added matrix row concatenation

--- examples/example.cpp
+++ examples/example.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 #include <algorithm>
 #include "../include/operator.hpp"
+#include "../include/algorithm.hpp"
 int main()
 {
     constexpr ce::ctmatrix<int, 3, 3> test{ 0, 1, 2, 3, 4, 5, 6, 7, 8 };
     constexpr ce::ctmatrix<int, 3, 3> test2(1);
-    constexpr auto i3 = ce::ctmatrix<int, 3,3>::identity();
-    constexpr auto res = (test+test2)*i3;
-    std::ranges::for_each(res.data, [](int a){std::cout<<a<<" ";});
+    constexpr auto i3 = test * test2;
+    constexpr auto res = ce::add_rows(test, i3);
+    int i=0;
+    std::ranges::for_each(res.data, [&](int a){
+                                        std::cout<<a<<" ";
+                                        if(i++ % 3 == 2)
+                                            std::cout<<'\n';
+                                    });
     return 0;
 }
--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -1,18 +1,28 @@
 #ifndef ALG_H
 #define ALG_H
+#include "matrix.hpp"
 #include "operator.hpp"
 #include "vector.hpp"
 
 namespace ce {
 
-//TODO finish implementing linear sovler
+// TODO finish implementing linear sovler
 template<numeric T, size_t N, size_t M>
-constexpr void swap_row(ctmatrix<T,N,M> &mat ,const size_t i, const size_t j)
+constexpr void swap_row(ctmatrix<T, N, M> &mat, const size_t i, const size_t j)
 {
-    std::array <int, M> temp{0};
-    std::copy(mat.begin()+M*i, mat.begin()+(M*i+M), temp.begin());
-    std::copy(mat.begin()+M*j, mat.begin()+M*j+M, mat.begin()+M*i);
-    std::copy(temp.begin(), temp.end(), mat.begin()+M*j);
+    std::array<int, M> temp{ 0 };
+    std::copy(mat.begin() + M * i, mat.begin() + (M * i + M), temp.begin());
+    std::copy(mat.begin() + M * j, mat.begin() + M * j + M, mat.begin() + M * i);
+    std::copy(temp.begin(), temp.end(), mat.begin() + M * j);
+}
+
+template<numeric T, size_t N, size_t M, size_t I>
+constexpr ctmatrix<T, I+N,M> add_rows(const ctmatrix<T, N,M> &lhs, const ctmatrix<T,I,M>& rhs) {
+    ctmatrix<T, I+N,M> ret;
+    std::copy(lhs.begin(), lhs.end(), ret.begin());
+    std::copy(rhs.begin(), rhs.end(), ret.begin()+(lhs.data.size()));
+    return ret;
+
 }
 
 }// namespace ce
